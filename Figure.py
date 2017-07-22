1441057a47947ceae2e7040949c68f906a91b3b9
Игорь <Sonic2455@yandex.ru>
2017-07-22 23:18:01 +0300

    Фигуры(Пока без равнобедренного треугольника)

--- Figure.py
+++ Figure.py
@@ -32,37 +32,31 @@ class Line(Polygon):
 
     def __init__(self, p1, p2):
         super().__init__(p1, p2)
-        self.p1 = p1
-        self.p2 = p2
 
 
 class Triangle(Polygon):
 
     def __init__(self, p1, p2, p3):
         super().__init__(p1, p2, p3)
-        self.p1 = p1
-        self.p2 = p2
-        self.p3 = p3
 
 
 class IsoscelesTriangle(Triangle):
 
     def __init__(self, p1, p2, l):
         super().__init__(p1, p2, 0)
-        self.l = l
 
 
 class Rectangle(Polygon):
 
     def __init__(self, l_d, r_u):
-        super().__init__(l_d, r_u)
-        self.l_d = l_d
-        self.r_u = r_u
+        l_u = Point(l_d.x, r_u.y)
+        r_d = Point(r_u.x, l_d.y)
+        super().__init__(l_d, l_u, r_u, r_d)
 
 
 class Square(Rectangle):
 
     def __init__(self, p, l):
-        super().__init__(p, 0)
-        self.p = p
-        self.l = l
+        l_d = p
+        r_u = Point(p.x + l, p.y + l)
+        super().__init__(l_d, r_u)
