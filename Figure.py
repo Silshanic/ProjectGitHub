b556bed4a969e0967dbbe36dc8b4e86e97800ae1
Игорь <Sonic2455@yandex.ru>
2017-07-24 23:21:58 +0300

    Фигуры(с равнобедренным треугольником)

--- Figure.py
+++ Figure.py
@@ -43,7 +43,20 @@ class Triangle(Polygon):
 class IsoscelesTriangle(Triangle):
 
     def __init__(self, p1, p2, l):
-        super().__init__(p1, p2, 0)
+        h_x = (p1.x + p2.x) / 2 # X координата середины
+        h_y = (p1.y + p2.y) / 2 # Y координата середины
+        ah = ((p1.x - h_x)**(2) + (p1.y - h_y)**(2))**(.5) # Длина половины основания
+        ch = ((l*l) - (ah * ah))**(.5) # Длина высоты
+        if p1.x == p2.x:
+            p3 = Point(h_x + ch, h_y)
+        elif p1.y == p2.y:
+            p3 = Point(h_x, h_y + ch)
+        else:
+            sin = abs((p1.y - p2.y) / 2) / ah
+            c_x = sin * ch
+            c_y = ((ch * ch) - (c_x * c_x))**(.5)
+            p3 = Point(h_x + c_x, h_y + c_y)
+        super().__init__(p1, p2, p3)
 
 
 class Rectangle(Polygon):
@@ -60,3 +73,4 @@ class Square(Rectangle):
         l_d = p
         r_u = Point(p.x + l, p.y + l)
         super().__init__(l_d, r_u)
+
